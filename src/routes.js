e221b303f903278ee4c84a07ad8d929e7301465b
Samanta Fluture <samantafluture@gmail.com>
2021-08-24 19:56:07 -0300

    config multer file upload

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 const routes = require('express').Router();
 const multer = require('multer');
+const multerConfig = require('./config/multer');
 
 routes.get('/', (req, res) => {
     return res.json({ message: 'Welcome to HostMyImg API!' });
 });
 
-routes.post('/posts', (req, res) => {
+routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
     return res.json();
 });
-
 
 module.exports = routes;
