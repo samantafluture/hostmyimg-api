085bd4f55b600c741ced9e6c632523cda05af68b
Samanta Fluture <samantafluture@gmail.com>
2021-08-24 22:38:29 -0300

    remove post from local and s3 storage

--- src/config/multer.js
+++ src/config/multer.js
@@ -4,6 +4,13 @@ const crypto = require('crypto');
 const aws = require('aws-sdk');
 const multerS3 = require('multer-s3');
 
+myConfig = new aws.Config();
+myConfig.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_DEFAULT_REGION
+});
+
 const storageTypes = {
     local: multer.diskStorage({
         destination: (req, file, cb) => {
@@ -21,7 +28,7 @@ const storageTypes = {
     }),
     s3: multerS3({
         s3: new aws.S3(),
-        bucket: 'hostmyimg',
+        bucket: process.env.BUCKET_NAME,
         contentType: multerS3.AUTO_CONTENT_TYPE,
         acl: 'public-read',
         key: (req, file, cb) => {
@@ -38,7 +45,7 @@ const storageTypes = {
 
 module.exports = {
     dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-    storage: storageTypes['s3'],
+    storage: storageTypes[process.env.STORAGE_TYPE],
     limits: {
         fileSize: 2 * 1024 * 1024
     },
--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
+
 const app = express();
 
 /**
  * Database setup
  */
-mongoose.connect('mongodb://localhost:27017/hostmyimg-api', {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(
+    '/files',
+    express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+);
 
 app.use(require('./routes'));
 
